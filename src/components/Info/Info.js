2de11e8c10c0867643c9ab6675e2def351219f06
Devon Bacon <devon.bacon@gmail.com>
2018-05-30 10:38:49 -0700

    Info - Created Info component placeholder for root directory

---
+++ src/components/Info/Info.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Info.css';
+
+const Info = () => {
+  return (
+    <div className="Info">
+      This app is designed to help you create and manage your notes.
+    </div>
+  )
+}
+
+export default Info;
