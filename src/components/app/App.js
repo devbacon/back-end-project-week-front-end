4d189e2994e12b8b5aa6800e5d93e17e04ce31e3
Devon Bacon <devon.bacon@gmail.com>
2018-05-30 10:39:33 -0700

    App.js - Implemented react routes for NoteHome and Info components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
 
+import Info from '../Info/Info';
 import NoteHome from '../NoteHome/NoteHome';
 
 import logo from '../../logo.svg';
@@ -19,15 +21,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Lambda Notes</h1>
-        </header>
-        <div className="App-body">
-          <NoteHome notes={this.state.notes} />
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to Lambda Notes</h1>
+          </header>
+          <div className="App-body">
+            <Route exact path="/" component={Info}/>
+            <Route path="/notes" render={() => 
+              <NoteHome notes={this.state.notes}/>
+            }/>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
