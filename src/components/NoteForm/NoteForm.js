43ccdd30f494313c346651d585f3afa673f65423
Devon Bacon <devon.bacon@gmail.com>
2018-05-30 14:54:21 -0700

    NoteForm.js - added methods to NoteForm

--- src/components/NoteForm/NoteForm.js
+++ src/components/NoteForm/NoteForm.js
@@ -11,6 +11,27 @@ class NoteForm extends Component {
     }
   }
 
+  componentDidMount() {
+    this.resizeInputs();
+  }
+
+  handleInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+    this.resizeInputs();
+  }
+
+  resizeInputs() {
+    const textareas = document.querySelectorAll('textarea');
+    textareas.forEach(textarea => {
+      textarea.style.height = "1px";
+      textarea.style.height = (textarea.scrollHeight)+"px";
+    });
+  }
+
+  resetInput() {
+    this.setState({ title: '', body: '' });
+  }
+
   render() {
     return (
       <form className="NoteForm">
