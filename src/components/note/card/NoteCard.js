8b042e09f6c209d89a2bbc89b40f83fe01f4d24e
Devon Bacon <devon.bacon@gmail.com>
2018-05-30 09:52:43 -0700

    NoteCard.js - renamed file and created NoteCard component

---
+++ src/components/note/card/NoteCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NoteCard.css'
+
+const NoteCard = props => {
+  return(
+    <div id={props.note._id} className='NoteCard'>
+      <div className='NoteCard-title'>{props.note.title}</div>
+      <div className='NoteCard-body'>{props.note.body}</div>
+    </div>
+  )
+}
+
+export default NoteCard;
