ddf063d3f7374a6f160ee1f7566fb14ecb9cffcc
Devon Bacon <devon.bacon@gmail.com>
2018-05-30 13:18:58 -0700

    NoteView.js - created NoteView component

---
+++ src/components/NoteView/NoteView.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './NoteView.css';
+
+class NoteView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+
+    axios.get(`https://some-awesome-lambda-notes-app.herokuapp.com/notes/${props.match.params.id}`)
+      .then(res => this.setState({ note: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return this.state.note ?
+      <div className="NoteView">
+        <div className="NoteView-title">{this.state.note.title}</div>
+        <div className="NoteView-body">{this.state.note.body}</div>
+      </div> : <div className="NoteView">Loading Note...</div>
+  }
+}
+
+export default NoteView;
