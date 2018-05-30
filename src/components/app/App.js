b721edb1e05b120b7b9b23fa14cb4940495c84d2
Devon Bacon <devon.bacon@gmail.com>
2018-05-30 10:16:06 -0700

    App.js - title change

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import NoteCard from '../note/card/NoteCard';
+import NoteHome from '../note/home/NoteHome';
 
 import logo from '../../logo.svg';
 import './App.css';
@@ -22,12 +22,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Lambda Notes</h1>
         </header>
         <div className="App-body">
-          {this.state.notes.map(note => {
-            return <NoteCard note={note} />
-          })}
+          <NoteHome notes={this.state.notes} />
         </div>
       </div>
     );
