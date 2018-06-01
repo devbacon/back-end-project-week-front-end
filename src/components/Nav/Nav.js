0e941881ebe276254d86022fb0bb4d8c763db6d5
Devon Bacon <devon.bacon@gmail.com>
2018-06-01 08:50:24 -0700

    Nav.js - pulled in test method from props

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 
-const Nav = () => {
+const Nav = props => {
   return (
     <div className="Nav">
       {/* Opens login drop down */}
-      <Link to=""><button className="Nav-login">Login</button></Link>
+      <Link to=""><button onClick={props.test}className="Nav-login">Login</button></Link>
       {/* Click button > click note > edit note > confirm */}
       <Link to="/note/form"><button className="Nav-create">Create</button></Link>
       {/* Click button > click note > edit note > confirm */}
