323a307e0d31717bbcb5b64ec5ca37bff9713dca
Devon Bacon <devon.bacon@gmail.com>
2018-06-01 11:51:12 -0700

    NoteView.js - added delete method and button links

--- src/components/NoteView/NoteView.js
+++ src/components/NoteView/NoteView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './NoteView.css';
 
@@ -12,11 +13,26 @@ class NoteView extends Component {
       .catch(err => console.log(err));
   }
 
+  deleteNote = () => {
+    const deleteData = { data: { id: this.state.note._id } };
+    axios.delete('https://some-awesome-lambda-notes-app.herokuapp.com/notes', deleteData) 
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
   render() {
     return this.state.note ?
-      <div className="NoteView">
-        <div className="NoteView-title">{this.state.note.title}</div>
-        <div className="NoteView-body">{this.state.note.body}</div>
+      <div>
+        <div className="NoteView">
+          <div className="NoteView-title">{this.state.note.title}</div>
+          <div className="NoteView-body">{this.state.note.body}</div>
+        </div>
+        <Link to={'/note/form'}>
+        <button className="NoteView-button">Edit</button>
+        </Link>
+        <Link to={'/notes'}>
+        <button className="NoteView-button" onClick={this.deleteNote}>Delete</button>
+        </Link>
       </div> : <div className="NoteView">Loading...</div>
   }
 }
